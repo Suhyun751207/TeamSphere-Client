864c423969f46d112139aeea1e41a998aef1f2d1
suhyun751207 <23s607h0659@gmail.com>
2025-09-15 03:03:05 +0900

    test : console.log

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Router from "./router/route";
 import './App.css';
 
 export default function App() {
+  console.log(process.env.REACT_APP_SERVER_URL)
   return (
     <div className="app">
       <div className="app-content">
