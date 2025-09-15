1d4867ccec73d7606847973c78dc19c9dc58162c
suhyun751207 <23s607h0659@gmail.com>
2025-09-15 09:26:19 +0900

    Update url

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_SERVER_URL || "http://localhost:8080/v1",
+    baseURL: "https://teamsphere-server-production.up.railway.app/v1/",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
