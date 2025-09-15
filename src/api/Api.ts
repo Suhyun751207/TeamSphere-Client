62c3a035d380e814993999576ec9838686b91413
suhyun751207 <23s607h0659@gmail.com>
2025-09-15 09:55:25 +0900

    feat

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://teamsphere-server-production.up.railway.app/v1/",
+    baseURL: process.env.REACT_APP_SERVER_URL || "https://teamsphere-server-production.up.railway.app/v1/",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
