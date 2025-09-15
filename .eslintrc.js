06ee510a0579851df7e35fb4a119e7aaea9f44ac
suhyun751207 <23s607h0659@gmail.com>
2025-09-15 10:24:14 +0900

    Create

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off', // 사용하지 않는 변수 경고 끔
+      'react-hooks/exhaustive-deps': 'off',      // useEffect 의존성 경고 끔
+      'jsx-a11y/anchor-is-valid': 'off',         // anchor href 경고 끔
+    },
+  };
+  
